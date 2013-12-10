4450276d3d39e492b6df5ff6a048eef696ae8124
AlexCherniak <alexalechko24@gmail.com>
2013-12-10 17:20:04 +0200

    first commit2

---
+++ TDD/src/ex3/CalculatorTester.java
@@ -0,0 +1,14 @@
+package ex3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
