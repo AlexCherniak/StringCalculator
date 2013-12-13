53588250afbfcc3ce49c2997e406a3a6faba925a
AlexCherniak <alexalechko24@gmail.com>
2013-12-13 16:54:47 +0200

    GREEN : Negative Numbers working

--- TDD/src/ex3/CalculatorTester.java
+++ TDD/src/ex3/CalculatorTester.java
@@ -9,66 +9,113 @@ public class CalculatorTester {
         @Test
         public void testEmptyString()
         {
-          StringCalculator calc = new StringCalculator();
-          int result = calc.Add("");
-          assertEquals(result, 0);
+                  StringCalculator calc = new StringCalculator();
+                  try
+                  {
+                      int result = calc.Add("");
+                      assertEquals(result, 0);
+                  }
+                  catch(Exception e)
+          {
+                   System.out.println(e);
+          }
+                  
+         
         }
         
         @Test
     public void testOneNumber()
     {
            StringCalculator calc = new StringCalculator();
-           int result = calc.Add("7");
-           assertEquals(result, 7);
-        }
+           try
+           {
+                   int result = calc.Add("7");
+                   assertEquals(result, 7);
+               }
+           catch(Exception e)
+           {
+                   System.out.println(e);
+           }
+    }
+        
         @Test
         public void testTwoNumbers()
         {
            StringCalculator calc = new StringCalculator();
-           int result = calc.Add("5,5");
-           assertEquals(result, 10);
+           try
+           {
+                   int result = calc.Add("5,5");
+                   assertEquals(result, 10);
+           }
+           catch(Exception e)
+           {
+                    System.out.println(e);
+           }
+          
     }
         
         @Test
     public void testNoLimitOnNumbers()
         {
             StringCalculator calc = new StringCalculator();
-            int result = calc.Add("5,5,5");
-            assertEquals(result, 15);
+            try
+            {
+                    int result = calc.Add("5,5,5");
+                    assertEquals(result, 15);
+            }
+            catch(Exception e)
+            {
+                    System.out.println(e);
+            }
     }
         
         @Test
     public void testLinesInsteadOfCommas()
         {
             StringCalculator calc = new StringCalculator();
-            int result = calc.Add("5,5\n5");
-            assertEquals(result, 15);
+            try
+            {
+                   int result = calc.Add("5,5\n5");
+                   assertEquals(result, 15);
+            }
+         
+            catch(Exception e)
+            {
+                    System.out.println(e);
+            }
     }
         
          @Test
      public void testSupportDifferentDelimiters()
          {
              StringCalculator calc = new StringCalculator();
-             int result = calc.Add("//;\n5;5,5\n5");
-             assertEquals(result,20);
+             try
+             {
+                     int result = calc.Add("//;\n5;5,5\n5");
+                 assertEquals(result,20);
+             }
+             catch(Exception e)
+             {
+                     System.out.println(e);
+             }
      }
          
      @Test
      public void testWithNegativeNumbers()
      {
              StringCalculator calc = new StringCalculator();
-            
+             
              try
              {
                      int result = calc.Add("-5,-5");
                      assertEquals(result, -10);
              }
-            
-           
+             
              catch(Exception e)
              {
                      System.out.println(e);
              }
+             
      }
 
 }
