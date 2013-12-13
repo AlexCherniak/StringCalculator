eefa9a2704dbb746c54ffc36893711c40e67144b
AlexCherniak <alexalechko24@gmail.com>
2013-12-13 15:29:13 +0200

    GREEN : Add working

--- TDD/src/ex3/CalculatorTester.java
+++ TDD/src/ex3/CalculatorTester.java
@@ -7,22 +7,25 @@ import org.junit.Test;
 public class CalculatorTester {
 	@Test
 	public void test() {
+	//	fail("Not yet implemented");
 	}
 	
 	@Test
 	public void addingEmptyStringReturns0() {
-	
 		StringCalculator calculator = new StringCalculator();
 		int result= calculator.addEmptyStringReturnZero("");
 		assertEquals(0,result);
 	}
+	
 	@Test
 	public void OneNum()
 	{
+
 				StringCalculator calculator = new StringCalculator();
-				int result= calculator.Add("5");
-				assertEquals(5,result);
+				int result= calculator.Add("7");
+				assertEquals(7,result);
 	}
+	
 	@Test
 	 public void TwoNum()
 	 {
--- TDD/src/ex3/StringCalculator.java
+++ TDD/src/ex3/StringCalculator.java
@@ -5,11 +5,21 @@ public class StringCalculator {
 	int addEmptyStringReturnZero(String numbers)
 	{
 		return 0;
-	}
-	int Add(String numbers)
+	}	
+	int Add(String input)
 	{
-		if(numbers.isEmpty())
-			 return 0;
-		return 5;
+		{
+			String[] numbers = input.split(",");
+			if((input)=="")
+				return 0;
+			else if(input.length() == 1)
+			 {
+				 return  Integer.parseInt(numbers[0]);
+			 }
+			else
+			{
+				return Integer.parseInt(numbers[0])+Integer.parseInt(numbers[0]);
+			}		
+		}
 	}
 }
