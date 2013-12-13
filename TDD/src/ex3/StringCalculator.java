3fb085a5b54836d331157eccfc67ac67f1266cdd
AlexCherniak <alexalechko24@gmail.com>
2013-12-13 16:20:25 +0200

    RED : (second test) No Limit Numbers

--- TDD/src/ex3/StringCalculator.java
+++ TDD/src/ex3/StringCalculator.java
@@ -1,23 +1,18 @@
 package ex3;
 
-
 public class StringCalculator {
-    private int stToInt(String string)
-    {
-            return Integer.parseInt(string);
-    }  
-    public int Add(String string)
-    {
-            if(string.isEmpty())
-                    return 0;
-            String[] numbers = string.split(",");
-             if(numbers.length == 1)
-                     return stToInt(string);
-            int sum=0;
-            for(int i=0;i<numbers.length;i++)
-            {
-                    sum+=stToInt(numbers[i]);
-            }
-            return sum;
-    }
+        private int stToInt(String string)//convert String to int
+        {
+                return Integer.parseInt(string);
+        }       
+        public int Add(String string)
+        {
+                if(string.isEmpty())
+                        return 0;
+                String[] numbers = string.split(",");
+                 if(numbers.length == 1)
+                         return stToInt(string);
+                return stToInt(numbers[0])+stToInt(numbers[1]);
+        }
+
 }
